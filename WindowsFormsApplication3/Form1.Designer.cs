409045987f07be60b2bac099868d77c9ebd6e720
John Earle <jcae6@hotmail.com>
2013-09-29 22:02:00 -0400

    Final Version

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtMonthly = new System.Windows.Forms.TextBox();
+            this.lbName = new System.Windows.Forms.Label();
+            this.lbId = new System.Windows.Forms.Label();
+            this.lbHours = new System.Windows.Forms.Label();
+            this.lbMonthly = new System.Windows.Forms.Label();
+            this.lbBonus = new System.Windows.Forms.Label();
+            this.txtBonus = new System.Windows.Forms.TextBox();
+            this.gbLang = new System.Windows.Forms.GroupBox();
+            this.radioFrench = new System.Windows.Forms.RadioButton();
+            this.radioEnglish = new System.Windows.Forms.RadioButton();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.button1 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.gbLang.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(12, 25);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.Leave += new System.EventHandler(this.txtName_Leave);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 68);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 1;
+            this.txtId.Leave += new System.EventHandler(this.txtId_Leave);
+            // 
+            // txtHours
+            // 
+            this.txtHours.Location = new System.Drawing.Point(12, 111);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(100, 20);
+            this.txtHours.TabIndex = 2;
+            this.txtHours.TextChanged += new System.EventHandler(this.txtHours_TextChanged);
+            this.txtHours.Leave += new System.EventHandler(this.txtHours_Leave);
+            // 
+            // txtMonthly
+            // 
+            this.txtMonthly.Location = new System.Drawing.Point(12, 154);
+            this.txtMonthly.Name = "txtMonthly";
+            this.txtMonthly.Size = new System.Drawing.Size(100, 20);
+            this.txtMonthly.TabIndex = 3;
+            this.txtMonthly.Enter += new System.EventHandler(this.txtMonthly_Enter);
+            this.txtMonthly.Leave += new System.EventHandler(this.txtMonthly_Leave);
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Location = new System.Drawing.Point(10, 9);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(84, 13);
+            this.lbName.TabIndex = 4;
+            this.lbName.Text = "Employee Name";
+            // 
+            // lbId
+            // 
+            this.lbId.AutoSize = true;
+            this.lbId.Location = new System.Drawing.Point(12, 52);
+            this.lbId.Name = "lbId";
+            this.lbId.Size = new System.Drawing.Size(67, 13);
+            this.lbId.TabIndex = 5;
+            this.lbId.Text = "Employee ID";
+            // 
+            // lbHours
+            // 
+            this.lbHours.AutoSize = true;
+            this.lbHours.Location = new System.Drawing.Point(12, 95);
+            this.lbHours.Name = "lbHours";
+            this.lbHours.Size = new System.Drawing.Size(103, 13);
+            this.lbHours.TabIndex = 6;
+            this.lbHours.Text = "Total Hours Worked";
+            // 
+            // lbMonthly
+            // 
+            this.lbMonthly.AutoSize = true;
+            this.lbMonthly.Location = new System.Drawing.Point(12, 138);
+            this.lbMonthly.Name = "lbMonthly";
+            this.lbMonthly.Size = new System.Drawing.Size(100, 13);
+            this.lbMonthly.TabIndex = 7;
+            this.lbMonthly.Text = "Total Monthly Sales";
+            // 
+            // lbBonus
+            // 
+            this.lbBonus.AutoSize = true;
+            this.lbBonus.Location = new System.Drawing.Point(164, 9);
+            this.lbBonus.Name = "lbBonus";
+            this.lbBonus.Size = new System.Drawing.Size(66, 13);
+            this.lbBonus.TabIndex = 8;
+            this.lbBonus.Text = "Sales Bonus";
+            // 
+            // txtBonus
+            // 
+            this.txtBonus.Location = new System.Drawing.Point(167, 25);
+            this.txtBonus.Name = "txtBonus";
+            this.txtBonus.ReadOnly = true;
+            this.txtBonus.Size = new System.Drawing.Size(100, 20);
+            this.txtBonus.TabIndex = 9;
+            this.txtBonus.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // gbLang
+            // 
+            this.gbLang.Controls.Add(this.radioFrench);
+            this.gbLang.Controls.Add(this.radioEnglish);
+            this.gbLang.Location = new System.Drawing.Point(12, 187);
+            this.gbLang.Name = "gbLang";
+            this.gbLang.Size = new System.Drawing.Size(99, 63);
+            this.gbLang.TabIndex = 10;
+            this.gbLang.TabStop = false;
+            this.gbLang.Text = "Language";
+            // 
+            // radioFrench
+            // 
+            this.radioFrench.AutoSize = true;
+            this.radioFrench.Location = new System.Drawing.Point(8, 40);
+            this.radioFrench.Name = "radioFrench";
+            this.radioFrench.Size = new System.Drawing.Size(65, 17);
+            this.radioFrench.TabIndex = 1;
+            this.radioFrench.TabStop = true;
+            this.radioFrench.Text = "Français";
+            this.radioFrench.UseVisualStyleBackColor = true;
+            // 
+            // radioEnglish
+            // 
+            this.radioEnglish.AutoSize = true;
+            this.radioEnglish.Location = new System.Drawing.Point(8, 19);
+            this.radioEnglish.Name = "radioEnglish";
+            this.radioEnglish.Size = new System.Drawing.Size(59, 17);
+            this.radioEnglish.TabIndex = 0;
+            this.radioEnglish.TabStop = true;
+            this.radioEnglish.Text = "English";
+            this.radioEnglish.UseVisualStyleBackColor = true;
+            this.radioEnglish.CheckedChanged += new System.EventHandler(this.radioEnglish_CheckedChanged);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(191, 52);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 11;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(190, 82);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 12;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication3.Properties.Resources.Confound;
+            this.pictureBox1.Location = new System.Drawing.Point(136, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(136, 139);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(128, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.gbLang);
+            this.Controls.Add(this.txtBonus);
+            this.Controls.Add(this.lbBonus);
+            this.Controls.Add(this.lbMonthly);
+            this.Controls.Add(this.lbHours);
+            this.Controls.Add(this.lbId);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.txtMonthly);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gbLang.ResumeLayout(false);
+            this.gbLang.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtMonthly;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Label lbId;
+        private System.Windows.Forms.Label lbHours;
+        private System.Windows.Forms.Label lbMonthly;
+        private System.Windows.Forms.Label lbBonus;
+        private System.Windows.Forms.TextBox txtBonus;
+        private System.Windows.Forms.GroupBox gbLang;
+        private System.Windows.Forms.RadioButton radioFrench;
+        private System.Windows.Forms.RadioButton radioEnglish;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,269 @@
+﻿/// <summary>
+/// App Name: Bonus Checker
+/// Author Name: John Earle
+/// App Creation Date: 26/09/2013
+/// 
+/// App Description: This tool will calcuate an employees sales bonus
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        char dollarSign = '$';
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtHours_TextChanged(object sender, EventArgs e)
+        {
+        }
+        
+        //Waits for validation to return true the calculates the sales bonus
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            if (validate())
+            {
+                char[] dollarSign = { '$' };
+                var unformattedMonthly = txtMonthly.Text.Trim(dollarSign);
+                var hours = Convert.ToDouble(txtHours.Text);
+                var monthly = Convert.ToDouble(unformattedMonthly);
+                txtBonus.Text = string.Format("${0:0.00}", Convert.ToString(Math.Round(((hours / 160) * (monthly * 0.02)), 2)));
+            }
+            else
+            {
+                MessageBox.Show("Please Fill The Boxes Properly");
+            }
+       }
+        // Validation is simple regex rules to search for . and char where there should be numbers so on and so forth
+        private bool validate()
+        {
+            if (valTxtName())
+            {
+                if (valTxtMonthly())
+                {
+                    if (valTxtName())
+                    {
+                    }
+                }
+                return true;
+            }
+            else{
+                MessageBox.Show("Please fill all boxes correctly");
+                return false; 
+            }
+        }
+
+        private bool valTxtName()
+        {
+            var regStr = @"^.*\d.*$";
+            if (txtName.Text == "" || Regex.IsMatch(txtName.Text, regStr))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private bool valTxtId()
+        {
+            var regStr = @"^.*\D.*$";
+            if (txtId.Text == "" || Regex.IsMatch(txtId.Text, regStr))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        private bool valTxtMonthly()
+        {
+            var regStr = @"^.*[^\d|^\.]\..*$";
+            var regStr2 = @"^.*\..*$";
+            var monthly = txtMonthly.Text.Trim(dollarSign);
+            if (monthly == "" || Regex.IsMatch(monthly, regStr))
+            {
+                return false;
+            }
+            else
+            {
+
+                if (Regex.IsMatch(monthly, regStr2))
+                {
+                    var formatedNumber = Math.Round(Convert.ToDouble(monthly), 2);
+                    txtMonthly.Text = string.Format("${0:0.00}", Convert.ToString(formatedNumber));
+                }
+                else
+                {
+                    var unformatedNumber = Math.Round(Convert.ToDouble(monthly),2);
+                    txtMonthly.Text = string.Format("${0:0.00}", Convert.ToString(unformatedNumber));
+                }
+                return true;
+            }
+        }
+        //When The name sales text box loses focus it runs valTxtName() for validation and changes the background color accordingly.
+        private void txtName_Leave(object sender, EventArgs e)
+        {
+
+            if (valTxtName())
+            {
+                txtName.BackColor = System.Drawing.Color.White;
+            }
+            else if(!valTxtName()){
+                txtName.BackColor = System.Drawing.Color.Red;
+            }
+        }
+       //Irritatingly ineffciate first attempt validation.
+        private void txtHours_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Convert.ToInt32(txtHours.Text) <= 160 & Convert.ToInt32(txtHours.Text) >= 0)
+                {
+
+
+                }
+                else
+                {
+                    MessageBox.Show("Please Enter A Value Between 160 and 0");
+                    txtHours.Text = "";
+                }
+            }
+            catch (System.FormatException)
+            {
+                MessageBox.Show("Please Enter A Value Between 160 and 0");
+            }
+        }
+        //When The ID text box loses focus it runs valTxtId() for validation and changes the background color accordingly.
+        private void txtId_Leave(object sender, EventArgs e)
+        {
+            if (valTxtId())
+            {
+                txtId.BackColor = System.Drawing.Color.White;
+            }
+            else if (!valTxtId())
+            {
+                txtId.BackColor = System.Drawing.Color.Red;
+            }
+        }
+        //When The monthly sales text box loses focus it runs valTxtMonthly() for validation and changes the background color accordingly.
+        private void txtMonthly_Leave(object sender, EventArgs e)
+        {
+            
+            if (valTxtMonthly())
+            {
+                txtMonthly.BackColor = System.Drawing.Color.White;
+            }
+            else if (!valTxtMonthly())
+            {
+                txtMonthly.BackColor = System.Drawing.Color.Red;
+            }
+        }
+        /*When The Monthly sales text box is entered the present value before user input is trimmed of the dollar sign
+         so it will not interfer with the regex later on        
+         */
+        private void txtMonthly_Enter(object sender, EventArgs e)
+        {
+            var unformattedMonthly = txtMonthly.Text.Trim(dollarSign);
+            txtMonthly.Text = unformattedMonthly;
+        }
+        //When the next button is clicked all fields are emptied
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            txtBonus.Text = "";
+            txtName.Text = "";
+            txtId.Text = "";
+            txtMonthly.Text = "";
+
+        }
+        /*
+         * Becuase there are two radio buttons in the event of any selection each ones state will be changed
+         * when changed it checks the users selection and sends true to changeLanguage() for english and vise versa.
+         * True and False not having any contextual value more so the small size of bool
+         */
+        private void radioEnglish_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioFrench.Checked == true)
+            {
+                changeLanguage(false);
+            }
+            else if (radioEnglish.Checked)
+            {
+                changeLanguage(true);
+            }
+        }
+        /*Change Language Holds all english and french labels in a 2 dimensonal array; 
+         * [0,*] being english and [1,*] being french
+         * the nth index in [*,nth] being a number from to seven of each visable title from top left to bottom too
+         * top right to bottom
+         * if parameter "Lang" is true english is set and vise-versa
+         */
+        private void changeLanguage(Boolean lang)
+        {
+            int arrayDepth;
+            String[,] Language = new String[2, 10];
+            Language[1, 0] = "Nom de l'employé";
+            Language[1, 1] = "Identification du salarié";
+            Language[1, 2] = "heures travaillées";
+            Language[1, 3] = "ventes mensuelles";
+            Language[1, 4] = "langue";
+            Language[1, 5] = "prime de vente";
+            Language[1, 6] = "calculer";
+            Language[1, 7] = "prochaine";
+            Language[0, 0] = "Employee Name";
+            Language[0, 1] = "Employee ID";
+            Language[0, 2] = "Total Hours Worked";
+            Language[0, 3] = "Total Monthly Sales";
+            Language[0, 4] = "Language";
+            Language[0, 5] = "Sales Bonus";
+            Language[0, 6] = "Calculate";
+            Language[0, 7] = "Next";
+            if (lang)
+            {
+                arrayDepth = 0;
+            }
+            else
+            {
+                arrayDepth = 1;
+            }
+            lbName.Text = Language[arrayDepth, 0];
+            lbId.Text = Language[arrayDepth, 1];
+            lbHours.Text = Language[arrayDepth, 2];
+            lbMonthly.Text = Language[arrayDepth, 3];
+            gbLang.Text = Language[arrayDepth, 4];
+            lbBonus.Text = Language[arrayDepth, 5];
+            btnCalc.Text = Language[arrayDepth, 6];
+            btnNext.Text = Language[arrayDepth, 7];
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+        }
+
+        private void printDocument1_PrintPage()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+    }
+}
